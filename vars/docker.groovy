92ff1f1a1571b0f9d79a62920e5b4e7d9be34172
naivedh <naivedh@gmail.com>
2025-01-23 17:29:29 +0530

    1

---
+++ vars/docker.groovy
@@ -0,0 +1,36 @@
+def call(String image = 'docker:latest') {
+    podTemplate(
+        showRawYaml: false,
+        yaml: '''
+              apiVersion: v1
+              kind: Pod
+              spec:
+                volumes:
+                - name: docker-socket
+                  emptyDir: {}
+                containers:
+                - name: docker
+                  image: ${image}
+                  readinessProbe:
+                    exec:
+                      command: [sh, -c, "ls -S /var/run/docker.sock"]
+                    initialDelaySeconds: 5
+                    periodSeconds: 5
+                  command:
+                  - sleep
+                  args:
+                  - 99d
+                  volumeMounts:
+                  - name: docker-socket
+                    mountPath: /var/run
+                - name: docker-daemon
+                  image: docker:dind
+                  securityContext:
+                    privileged: true
+                  command: ["dockerd"]
+                  volumeMounts:
+                  - name: docker-socket
+                    mountPath: /var/run
+        '''
+    )
+}
