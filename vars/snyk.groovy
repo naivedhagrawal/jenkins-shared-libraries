1b9353e36ebba75fb251ae9ab4f4cc8a883b9714
naivedh <naivedh@gmail.com>
2025-01-27 17:59:43 +0530

    1

--- vars/snyk.groovy
+++ vars/snyk.groovy
@@ -1,14 +1,28 @@
 // @Library('Shared-Libraries') _
 // Fuction call --> pod(image:version)
 
-def call(String image = 'none') {
+def call(String projectType) {
+    def snykImage = ''
+    switch (projectType) {
+        case 'maven':
+            snykImage = 'snyk/snyk:maven'
+            break
+        case 'node':
+            snykImage = 'snyk/snyk:node'
+            break
+        case 'python':
+            snykImage = 'snyk/snyk:python'
+            break
+        default:
+            error "Unsupported project type: ${projectType}"
+    }
     return """
         apiVersion: v1
         kind: Pod
         spec:
           containers:
           - name: snyk
-            image: ${image}
+            image: ${snykImage}
             imagePullPolicy: Always
             command:
             - cat
