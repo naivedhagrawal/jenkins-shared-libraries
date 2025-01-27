00d9bc1d66a32185ff0be03073a14bb1ce66e06f
naivedh <naivedh@gmail.com>
2025-01-27 14:30:47 +0530

    1

--- vars/snyk.groovy
+++ vars/snyk.groovy
@@ -1,17 +1,30 @@
 // @Library('Shared-Libraries') _
 // Fuction call --> pod()
+// naivedh/snyk-image:latest
 
-def call() {
+def snyk() {
     return """
-        apiVersion: v1
-        kind: Pod
-        spec:
-          containers:
-          - name: snyk
-            image: naivedh/snyk-image:latest
-            imagePullPolicy: Always
-            command:
-            - cat
-            tty: true
-        """
+apiVersion: v1
+kind: Pod
+metadata:
+  name: snyk-pod
+spec:
+  containers:
+  - name: snyk
+    image: naivedh/snyk-image:latest # Your Snyk image
+    imagePullPolicy: IfPresent # or Always if you want to always pull the latest
+    volumeMounts:
+      - name: workspace-volume
+        mountPath: /app
+    env:
+      - name: SNYK_TOKEN
+        valueFrom:
+          secretKeyRef:
+            name: snyk-secret # Name of your Kubernetes Secret
+            key: snyk-token
+  volumes:
+  - name: workspace-volume
+    persistentVolumeClaim:
+      claimName: workspace-pvc # Name of your PersistentVolumeClaim
+"""
 }
