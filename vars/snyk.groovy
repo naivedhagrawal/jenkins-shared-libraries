59cb8e28d5e8ecde4f656ef91e650788d4d98cfa
naivedh <naivedh@gmail.com>
2025-01-27 19:43:03 +0530

    1

--- vars/snyk.groovy
+++ vars/snyk.groovy
@@ -44,8 +44,8 @@ def call(String projectType, boolean runImageScan = false, String imageName = ''
             - -c
             - |
               withCredentials([string(credentialsId: 'SNYK_TOKEN', variable: 'SNYK_TOKEN')]) {
-                  snyk auth \\\\$SNYK_TOKEN && \\
-                  ${scanCommands.replaceAll('\n', ' \\\\n')}
+                  snyk auth \$SNYK_TOKEN && \\
+                  ${scanCommands}
               }
             tty: true
     """
