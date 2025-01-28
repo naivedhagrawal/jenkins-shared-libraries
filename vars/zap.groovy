fc45c31a03b743e0e84ee3e02861abddff44122a
naivedh <naivedh@gmail.com>
2025-01-29 01:35:20 +0530

    1

--- vars/zap.groovy
+++ vars/zap.groovy
@@ -11,7 +11,7 @@ spec:
     ports:
     - containerPort: 8080 # ZAP API port
     - containerPort: 8090 # ZAP UI port (optional)
-    args: ["-daemon", "-port", "8080", "-host", "0.0.0.0"] # Correct way to pass arguments
+    args: ["/zap.sh", "-daemon", "-port", "8080", "-host", "0.0.0.0"]
     volumeMounts: # If you need persistent storage for ZAP data
       - name: zap-volume
         mountPath: /zap/wrk/
