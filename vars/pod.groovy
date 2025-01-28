5e9e3d92fef4828d2957a2342f6dbbb3d070b450
naivedh <naivedh@gmail.com>
2025-01-28 16:51:29 +0530

    1

--- vars/pod.groovy
+++ vars/pod.groovy
@@ -1,10 +1,10 @@
-def call(String image = 'jenkins/inbound-agent:latest', boolean showRawYaml = false) {
+def call(String name = 'jnlp', String image = 'jenkins/inbound-agent:latest', boolean showRawYaml = false) {
     return """
 apiVersion: v1
 kind: Pod
 spec:
   containers:
-    - name: jnlp
+    - name: ${name}
       image: "${image}"
       imagePullPolicy: Always
       command:
