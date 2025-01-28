b76f8f67c83ce22b66925d425a09d0e88f20e2aa
naivedh <naivedh@gmail.com>
2025-01-29 01:30:46 +0530

    1

--- vars/zap.groovy
+++ vars/zap.groovy
@@ -7,11 +7,11 @@ metadata:
 spec:
   containers:
   - name: zap
-    image: owasp/zap-stable  // Or owasp/zap-weekly
+    image: owasp/zap-stable  # Or owasp/zap-weekly
     ports:
-    - containerPort: 8080 // ZAP API port
-    - containerPort: 8090 // ZAP UI port (optional, for debugging)
-    command: ["/zap.sh", "-daemon", "-port", "8080", "-host", "0.0.0.0"] // Start ZAP in daemon mode
+    - containerPort: 8080 # ZAP API port
+    - containerPort: 8090 # ZAP UI port (optional, for debugging)
+    command: ["/zap.sh", "-daemon", "-port", "8080", "-host", "0.0.0.0"] # Start ZAP in daemon mode
     volumeMounts:
       - name: zap-volume
         mountPath: /zap/wrk/
