f694652aca4b06abfb833df56a66232a8387f949
naivedh <naivedh@gmail.com>
2025-01-29 01:38:52 +0530

    1

--- vars/zap.groovy
+++ vars/zap.groovy
@@ -3,11 +3,20 @@ def call() {
 apiVersion: v1
 kind: Pod
 metadata:
-  name: zap
+  name: zap-pod
 spec:
   containers:
   - name: zap
-    image: zaproxy/zap-stable
-    args: ["/zap.sh", "-daemon"]
+    image: zaproxy/zap-stable  # Correct image name
+    ports:
+    - containerPort: 8080 # ZAP API port
+    - containerPort: 8090 # ZAP UI port (optional)
+    args: ["/zap/zap.sh", "-daemon", "-port", "8080", "-host", "0.0.0.0"]
+    volumeMounts: # If you need persistent storage for ZAP data
+      - name: zap-volume
+        mountPath: /zap/wrk/
+  volumes:
+  - name: zap-volume
+    emptyDir: {}
 """
 }
