4b27c6321bd1d8adb45e832d3494a7fcdc676e20
naivedh <naivedh@gmail.com>
2025-01-29 22:04:09 +0530

    1

---
+++ vars/zap.groovy
@@ -0,0 +1,16 @@
+def call(String name = 'jnlp', String image = 'jenkins/inbound-agent:latest') {
+    return """
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    component: zap
+spec:
+  containers:
+  - name: zap
+    image: zaproxy/zap-stable
+    args: ["zap.sh", "-daemon", "-host", "0.0.0.0", "-port", "8080"]
+    ports:
+    - containerPort: 8080
+    """
+}
