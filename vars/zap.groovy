828bd6c519e8f32d1a2003858209265850f20775
naivedh <naivedh@gmail.com>
2025-01-29 22:59:45 +0530

    1

--- vars/zap.groovy
+++ vars/zap.groovy
@@ -6,11 +6,17 @@ metadata:
   labels:
     component: zap
 spec:
+  volumes:
+    - name: zap-data
+      emptyDir: {}
   containers:
   - name: zap
     image: zaproxy/zap-stable
     args: ["zap.sh", "-daemon", "-host", "0.0.0.0", "-port", "8080"]
     ports:
     - containerPort: 8080
+    volumeMounts:
+    - name: zap-data
+      mountPath: /zap/.ZAP_HOME
     """
 }
