84bb28752dc12e8bcd4b7efde2f5b28b623a066e
naivedh <naivedh@gmail.com>
2025-01-30 00:02:12 +0530

    1

--- vars/zap.groovy
+++ vars/zap.groovy
@@ -1,26 +1,26 @@
 def call() {
-    return """apiVersion: v1
+  return """apiVersion: v1
 kind: Pod
 spec:
   containers:
-    - name: zap
-      image: zaproxy/zap-stable:latest
-      securityContext:
-        runAsUser: 1000  # Assuming 'zap' user is mapped to UID 1000, adjust accordingly
-      volumeMounts:
-        - name: temp-volume
-          mountPath: /zap/wrk  # Mounting /tmp on the host to /zap/wrk in the container
-        - name: output-volume
-          mountPath: /zap/output  # Mount an output directory
-      tty: true  # Enable TTY for the container
-  volumes:
+  - name: zap
+    image: zaproxy/zap-stable:latest
+    securityContext:
+    runAsUser: 1000
+    volumeMounts:
     - name: temp-volume
-      hostPath:
-        path: /tmp
-        type: Directory
+      mountPath: /zap/wrk
     - name: output-volume
-      hostPath:
-        path: /tmp/zap-output  # Adjust this to where you want the output files stored
-        type: Directory
+      mountPath: /zap/output
+    tty: true
+  volumes:
+  - name: temp-volume
+    hostPath:
+    path: /tmp
+    type: Directory
+  - name: output-volume
+    hostPath:
+    path: /tmp/zap-output
+    type: Directory
 """
 }
