6895fa6bce3aaab2abcae6ddb5cd6e8b8b2ec0cb
naivedh <naivedh@gmail.com>
2025-01-29 15:25:46 +0530

    1

--- vars/zap.groovy
+++ vars/zap.groovy
@@ -1,41 +1,22 @@
 def call() {
-    return '''apiVersion: v1
+    return '''
+apiVersion: v1
 kind: Pod
 metadata:
   name: zap-pod
 spec:
   containers:
-    - name: zap
+    - name: zap-container
       image: zaproxy/zap-stable
+      command: ["zap-webui"]
       ports:
-        - containerPort: 8080  # Default port ZAP runs on
-      env:
-        - name: ZAP_API_KEY
-          value: "-5IrWmmH4xuGCAYjRI1CwOSoxNAHK1ErXfZ4KJ9vMv4"  # Set an API key if required
-      command:
-        - "zap.sh"
-        - "-daemon"  # Run ZAP in daemon mode (headless)
-      args:
-        - "-host"
-        - "0.0.0.0"  # Allow access from any IP
-        - "-port"
-        - "8080"  # Port ZAP listens on
-        - "-config"
-        - "connection.requestTimeoutInMs=60000"  # Optional: Set request timeout to 60 seconds
-
----
-apiVersion: v1
-kind: Service
-metadata:
-  name: zap-service
-  namespace: devops-tools
-spec:
-  selector:
-    app: zap-pod  # This should match the label on the pod
-  ports:
-    - protocol: TCP
-      port: 8081  # Exposing ZAP on port 8081 (change from 8080 to avoid conflict)
-      targetPort: 8080  # The port the container is listening on
-  clusterIP: None  # Optional: for headless service, you can set it to None if you only want pod-to-pod communication
+        - containerPort: 8080
+        - containerPort: 8081
+      volumeMounts:
+        - name: zap-home
+          mountPath: /home/zap/.ZAP
+  volumes:
+    - name: zap-home
+      emptyDir: {}
 '''
 }
