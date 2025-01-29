4889b9d78aecebcba9df715af571f0ed8f3df111
naivedh <naivedh@gmail.com>
2025-01-29 14:10:55 +0530

    1

--- vars/zap.groovy
+++ vars/zap.groovy
@@ -1,6 +1,8 @@
 def call() {
     return '''apiVersion: v1
 kind: Pod
+metadata:
+  name: zap-pod
 spec:
   containers:
     - name: zap
@@ -19,5 +21,20 @@ spec:
         - "-port"
         - "8080"  # Port ZAP listens on
         - "-config"
-        - "connection.requestTimeoutInMs=60000"  # Optional: Set request timeout to 60 seconds'''
+        - "connection.requestTimeoutInMs=60000"  # Optional: Set request timeout to 60 seconds
+
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: zap-service
+spec:
+  selector:
+    app: zap-pod  # This should match the label on the pod
+  ports:
+    - protocol: TCP
+      port: 8081  # Exposing ZAP on port 8081 (change from 8080 to avoid conflict)
+      targetPort: 8080  # The port the container is listening on
+  clusterIP: None  # Optional: for headless service, you can set it to None if you only want pod-to-pod communication
+'''
 }
