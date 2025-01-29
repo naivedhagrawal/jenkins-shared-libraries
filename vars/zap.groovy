54b3937b12ae2c45904eafde2c9ee8c63c300ba5
naivedh <naivedh@gmail.com>
2025-01-29 23:59:35 +0530

    1

--- vars/zap.groovy
+++ vars/zap.groovy
@@ -5,18 +5,22 @@ spec:
   containers:
     - name: zap
       image: zaproxy/zap-stable:latest
-      command: ["zap-baseline.py"]
-      args: ["-t", "${TARGET_URL}"]
       securityContext:
         runAsUser: 1000  # Assuming 'zap' user is mapped to UID 1000, adjust accordingly
       volumeMounts:
         - name: temp-volume
-          mountPath: /zap/wrk
+          mountPath: /zap/wrk  # Mounting /tmp on the host to /zap/wrk in the container
+        - name: output-volume
+          mountPath: /zap/output  # Mount an output directory
       tty: true  # Enable TTY for the container
   volumes:
     - name: temp-volume
       hostPath:
         path: /tmp
+        type: Directory
+    - name: output-volume
+      hostPath:
+        path: /tmp/zap-output  # Adjust this to where you want the output files stored
         type: Directory
 """
 }
