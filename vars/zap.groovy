0b9e698f1a8c6d5ade4a2044db1b54cb93de8a4b
naivedh <naivedh@gmail.com>
2025-01-30 23:51:01 +0530

    1

--- vars/zap.groovy
+++ vars/zap.groovy
@@ -2,15 +2,31 @@ def call() {
   return """
         apiVersion: v1
         kind: Pod
-        metadata:
-          name: zap
-          labels:
-            app: zap
         spec:
           containers:
+          - name: zap-daemon
+            image: zaproxy/zap-bare:latest
+            command: ["/zap/zap.sh", "-daemon", "-host", "127.0.0.1", "-port", "8080"]
+            securityContext:
+              runAsUser: 1000
+              readOnlyRootFilesystem: false
+              fsGroup: 1000
+            volumeMounts:
+            - name: zap-data
+              mountPath: /zap/reports
+              subPath: reports
+              readOnly: false
+            - name: zap-wrk
+              mountPath: /zap/wrk/data
+              subPath: data
+              readOnly: false
+            - name: zap-home
+              mountPath: /home/zap/custom_data
+              subPath: custom_data
+              readOnly: false
+            tty: true
           - name: zap
             image: zaproxy/zap-bare:latest
-            command: ["/zap/zap.sh", "-daemon", "-host", "127.0.0.1", "-port", "8080" , '-home' , '/home/zap/custom_data' ]
             securityContext:
               runAsUser: 1000
               readOnlyRootFilesystem: false
