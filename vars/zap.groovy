85d3553ef7f740a03ba0d9edcf6e20e47227e813
naivedh <naivedh@gmail.com>
2025-01-30 11:54:26 +0530

    1

--- vars/zap.groovy
+++ vars/zap.groovy
@@ -2,26 +2,29 @@ def call() {
   return """
         apiVersion: v1
         kind: Pod
+        metadata:
+          name: zap
         spec:
           containers:
           - name: zap
             image: zaproxy/zap-stable:latest
             securityContext:
               runAsUser: 1000
+              readOnlyRootFilesystem: false
             volumeMounts:
-            - name: zap-volume
-              mountPath: /zap
-            - name: home
-              mountPath: /home/zap
+            - name: zap-data
+              mountPath: /zap/reports
+              subPath: reports
+            - name: zap-home
+              mountPath: /home/zap/custom_data  # `/home/zap` को प्रभावित किए बिना अलग डायरेक्टरी माउंट करें
+              subPath: custom_data
             tty: true
           volumes:
-          - name: zap-volume
-            hostPath:
-              path: /zap
-              type: DirectoryOrCreate
-          - name: home
-            hostPath:
-              path: /home/zap
-              type: DirectoryOrCreate
+          - name: zap-data
+            emptyDir: {}
+          - name: zap-home
+            emptyDir: {}  # या PVC के लिए PersistentVolumeClaim उपयोग करें
+          restartPolicy: Always
+
 """
 }
