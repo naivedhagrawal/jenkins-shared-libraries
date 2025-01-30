23adcda383031b563f38b58f9dda03a6e2cc84c5
naivedh <naivedh@gmail.com>
2025-01-30 11:49:12 +0530

    1

--- vars/zap.groovy
+++ vars/zap.groovy
@@ -1,22 +1,27 @@
 def call() {
-  return """apiVersion: v1
-kind: Pod
-spec:
-  containers:
-  - name: zap
-    image: zaproxy/zap-stable:latest
-    securityContext:
-    runAsUser: 1000
-    volumeMounts:
-    - name: temp-volume
-      mountPath: /zap
-    - name: output-volume
-      mountPath: /home/zap
-    tty: true
-  volumes:
-  - name: temp-volume
-    emptyDir: {}
-  - name: output-volume
-    emptyDir: {}
+  return """
+        apiVersion: v1
+        kind: Pod
+        spec:
+          containers:
+          - name: zap
+            image: zaproxy/zap-stable:latest
+            securityContext:
+              runAsUser: 1000
+            volumeMounts:
+            - name: zap-volume
+              mountPath: /zap
+            - name: home
+              mountPath: /home/zap
+            tty: true
+          volumes:
+          - name: zap-volume
+            hostPath:
+              path: /zap
+              type: DirectoryOrCreate
+          - name: home
+            hostPath:
+              path: /home/zap
+              type: DirectoryOrCreate
 """
 }
