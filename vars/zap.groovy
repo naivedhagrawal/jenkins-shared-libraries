94aaf4c91b169246b087131249b17fae5c28e751
naivedh <naivedh@gmail.com>
2025-01-31 21:40:33 +0530

    1

--- vars/zap.groovy
+++ vars/zap.groovy
@@ -12,6 +12,40 @@ spec:
         runAsGroup: 1000
         readOnlyRootFilesystem: false
         fsGroup: 1000
+      volumeMounts:
+        - name: zap-wrk
+          mountPath: /zap/wrk
+          subPath: wrk
+          readOnly: false
       tty: true
+
+  volumes:
+    - name: zap-wrk
+      persistentVolumeClaim:
+        claimName: zap-pvc
+---
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: zap-pv
+spec:
+  capacity:
+    storage: 1Gi
+  accessModes:
+    - ReadWriteOnce
+  hostPath:
+    path: /home/data/sonar
+    type: DirectoryOrCreate
+---
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: zap-pvc
+spec:
+  accessModes:
+    - ReadWriteOnce
+  resources:
+    requests:
+      storage: 1Gi
 """
 }
