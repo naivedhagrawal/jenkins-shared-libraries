1dabcde7c263fff9ee13ac52cab1708b052f2613
naivedh <naivedh@gmail.com>
2025-01-31 21:28:23 +0530

    1

--- vars/zap.groovy
+++ vars/zap.groovy
@@ -5,34 +5,13 @@ kind: Pod
 spec:
   containers:
     - name: zap
-      image: naivedh/owasp-zap:latest
-      command: ['cat']
+      image: zaproxy/zap-stable
+      command: ["/bin/sh", "-c", "tail -f /dev/null"]
       securityContext:
         runAsUser: 1000
         runAsGroup: 1000
         readOnlyRootFilesystem: false
         fsGroup: 1000
-      volumeMounts:
-        - name: zap-data
-          mountPath: /zap/reports
-          subPath: reports
-          readOnly: false
-        - name: zap-wrk
-          mountPath: /zap/wrk/data
-          subPath: data
-          readOnly: false
-        - name: zap-home
-          mountPath: /home/zap/custom_data
-          subPath: custom_data
-          readOnly: false
       tty: true
-
-  volumes:
-    - name: zap-data
-      emptyDir: {}
-    - name: zap-home
-      emptyDir: {}
-    - name: zap-wrk
-      emptyDir: {}
 """
 }
