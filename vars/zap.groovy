793c6feeba2a6707a3387edbf5b77e854eade437
naivedh <naivedh@gmail.com>
2025-01-31 22:05:50 +0530

    1

--- vars/zap.groovy
+++ vars/zap.groovy
@@ -2,30 +2,41 @@ def call() {
     return """
 apiVersion: v1
 kind: Pod
+metadata:
+  name: zap-pod
 spec:
   containers:
     - name: zap
       image: naivedh/owasp-zap:latest
-      command: ["/bin/sh", "-c", "tail -f /dev/null"]
+      command: ['cat']
       securityContext:
         runAsUser: 1000
-        runAsGroup: 1000
         readOnlyRootFilesystem: false
         fsGroup: 1000
       volumeMounts:
-        - name: zap-wrk
-          mountPath: /zap/wrk
-          subPath: wrk
+        - name: zap-data
+          mountPath: /zap/reports
+          subPath: reports
           readOnly: false
         - name: zap-wrk
-          mountPath: /home/zap
-          subPath: zap
+          mountPath: /zap/wrk/data
+          subPath: data
+          readOnly: false
+        - name: zap-home
+          mountPath: /home/zap/custom_data
+          subPath: custom_data
           readOnly: false
       tty: true
 
   volumes:
+    - name: zap-data
+      emptyDir: {}
+    - name: zap-home
+      emptyDir: {}
     - name: zap-wrk
-      persistentVolumeClaim:
-        claimName: zap-pv-claim
+      emptyDir: {}
+
+  restartPolicy: Always
+
 """
 }
