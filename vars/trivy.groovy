c4f73937ed7a7046a5f39d3aab88b410aa029392
naivedh <naivedh@gmail.com>
2025-02-01 11:01:52 +0530

    1

--- vars/trivy.groovy
+++ vars/trivy.groovy
@@ -2,20 +2,46 @@ def call() {
     return """
 apiVersion: v1
 kind: Pod
-metadata:
-  name: trivy-scanner
 spec:
+  volumes:
+  - name: docker-socket
+    emptyDir: {}
   containers:
-  - name: trivy
-    image: aquasec/trivy:latest
-    command: ["cat"]
-    tty: true
-  - name: dind
+  - name: docker
+    image: docker:latest
+    readinessProbe:
+      exec:
+        command: [sh, -c, "ls -S /var/run/docker.sock"]
+      initialDelaySeconds: 5
+      periodSeconds: 5
+    command:
+    - sleep
+    args:
+    - 99d
+    volumeMounts:
+    - name: docker-socket
+      mountPath: /var/run
+  - name: docker-daemon
     image: docker:dind
     securityContext:
       privileged: true
-    env:
-    - name: DOCKER_TLS_CERTDIR
-      value: ""
-    """
+    command: ["dockerd"]
+    volumeMounts:
+    - name: docker-socket
+      mountPath: /var/run
+  - name: trivy
+    image: aquasec/trivy:latest
+    command: ["trivy"]
+    args:
+    - "--quiet"
+    - "--no-progress"
+    - "--format"
+    - "json"
+    - "--output"
+    - "/tmp/trivy-report.json"
+    - "/var/run/docker.sock"
+    volumeMounts:
+    - name: docker-socket
+      mountPath: /var/run
+"""
 }
