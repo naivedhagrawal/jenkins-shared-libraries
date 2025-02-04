d93e10380c0b832beab475177d7115eeca400798
naivedh <naivedh@gmail.com>
2025-02-04 19:06:46 +0530

    1

--- vars/JsonToSarif.groovy
+++
@@ -1,7 +0,0 @@
-def call(String inputJson, String outputSarif) {
-    sh """
-        def jsonToSarif = libraryResource('zap_json_to_sarif.py')
-        writeFile file: 'zap_json_to_sarif.py', text: jsonToSarif
-        python3 zap_json_to_sarif.py ${inputJson} ${outputSarif}
-    """
-}
