c2b731ebad9ffd460979976cf1e8686196cae526
naivedh <naivedh@gmail.com>
2025-02-04 18:58:29 +0530

    1

--- vars/JsonToSarif.groovy
+++ vars/JsonToSarif.groovy
@@ -1,5 +1,6 @@
 def call(String inputJson, String outputSarif) {
     sh """
+        sh "cp ${JENKINS_HOME}/workspace/${JOB_NAME}@libs/k8s-shared-lib/resources/zap_json_to_sarif.py ."
         python3 zap_json_to_sarif.py ${inputJson} ${outputSarif}
     """
 }
