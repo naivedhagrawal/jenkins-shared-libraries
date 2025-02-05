c611c162e86bc4811a6d03fc6087fef7c4010fd3
naivedh <naivedh@gmail.com>
2025-02-05 15:22:24 +0530

    1

--- vars/securityscan.groovy
+++ vars/securityscan.groovy
@@ -3,96 +3,104 @@ def call(Map params = [:]) {
     def OWASP_DEP_REPORT = 'owasp-dep-report.sarif'
     def SEMGREP_REPORT = 'semgrep-report.sarif'
 
-    if (params.gitleak) {
-        stage('Gitleak Check') {
-            agent {
-                kubernetes {
-                    yaml pod('gitleak', 'zricethezav/gitleaks')
-                    showRawYaml false
+    pipeline {
+        agent none
+
+        stages {
+            stage('Gitleak Check') {
+                when {
+                    expression { params.gitleak }
                 }
-            }
-            steps {
-                script {
-                    container('gitleak') {
-                        sh """
-                            gitleaks detect \
-                                --source=. \
-                                --report-path=${GITLEAKS_REPORT} \
-                                --report-format sarif \
-                                --exit-code=0
-                        """
-                        recordIssues(
-                            enabledForFailure: true,
-                            tool: sarif(
-                                pattern: "${GITLEAKS_REPORT}",
-                                id: "gitLeaks-SARIF",
-                                name: "GitLeaks-Report"
-                            )
-                        )
-                        archiveArtifacts artifacts: "${GITLEAKS_REPORT}"
+                agent {
+                    kubernetes {
+                        yaml pod('gitleak', 'zricethezav/gitleaks')
+                        showRawYaml false
                     }
                 }
-            }
-        }
-    }
-
-    if (params.owaspdependency) {
-        stage('OWASP Dependency Check') {
-            agent {
-                kubernetes {
-                    yaml pod('owasp', 'naivedh/owasp-dependency:latest')
-                    showRawYaml false
-                }
-            }
-            steps {
-                script {
-                    container('owasp') {
-                        withCredentials([string(credentialsId: 'NVD_API_KEY', variable: 'NVD_API_KEY')]) {
+                steps {
+                    script {
+                        container('gitleak') {
                             sh """
-                                dependency-check --scan . \
-                                    --format SARIF \
-                                    --out ${OWASP_DEP_REPORT} \
-                                    --nvdApiKey ${env.NVD_API_KEY}
+                                gitleaks detect \
+                                    --source=. \
+                                    --report-path=${GITLEAKS_REPORT} \
+                                    --report-format sarif \
+                                    --exit-code=0
                             """
                             recordIssues(
                                 enabledForFailure: true,
                                 tool: sarif(
-                                    pattern: "${OWASP_DEP_REPORT}",
-                                    id: "owasp-dependency-check-SARIF",
-                                    name: "OWASP Dependency Check Report"
+                                    pattern: "${GITLEAKS_REPORT}",
+                                    id: "gitLeaks-SARIF"
                                 )
                             )
-                            archiveArtifacts artifacts: "${OWASP_DEP_REPORT}"
+                            archiveArtifacts artifacts: "${GITLEAKS_REPORT}"
                         }
                     }
                 }
             }
-        }
-    }
 
-    if (params.semgrep) {
-        stage('Semgrep Scan') {
-            agent {
-                kubernetes {
-                    yaml pod('semgrep', 'returntocorp/semgrep')
-                    showRawYaml false
+            stage('OWASP Dependency Check') {
+                when {
+                    expression { params.owaspdependency }
+                }
+                agent {
+                    kubernetes {
+                        yaml pod('owasp', 'naivedh/owasp-dependency:latest')
+                        showRawYaml false
+                    }
+                }
+                steps {
+                    script {
+                        container('owasp') {
+                            withCredentials([string(credentialsId: 'NVD_API_KEY', variable: 'NVD_API_KEY')]) {
+                                sh """
+                                    dependency-check --scan . \
+                                        --format SARIF \
+                                        --out ${OWASP_DEP_REPORT} \
+                                        --nvdApiKey ${env.NVD_API_KEY}
+                                """
+                                recordIssues(
+                                    enabledForFailure: true,
+                                    tool: sarif(
+                                        pattern: "${OWASP_DEP_REPORT}",
+                                        id: "owasp-dependency-check-SARIF",
+                                        name: "OWASP Dependency Check Report"
+                                    )
+                                )
+                                archiveArtifacts artifacts: "${OWASP_DEP_REPORT}"
+                            }
+                        }
+                    }
                 }
             }
-            steps {
-                script {
-                    container('semgrep') {
-                        sh """
-                            semgrep --config=auto --sarif --output ${SEMGREP_REPORT} .
-                        """
-                        recordIssues(
-                            enabledForFailure: true,
-                            tool: sarif(
-                                pattern: "${SEMGREP_REPORT}",
-                                id: "semgrep-SARIF",
-                                name: "Semgrep Report"
+
+            stage('Semgrep Scan') {
+                when {
+                    expression { params.semgrep }
+                }
+                agent {
+                    kubernetes {
+                        yaml pod('semgrep', 'returntocorp/semgrep')
+                        showRawYaml false
+                    }
+                }
+                steps {
+                    script {
+                        container('semgrep') {
+                            sh """
+                                semgrep --config=auto --sarif --output ${SEMGREP_REPORT} .
+                            """
+                            recordIssues(
+                                enabledForFailure: true,
+                                tool: sarif(
+                                    pattern: "${SEMGREP_REPORT}",
+                                    id: "semgrep-SARIF",
+                                    name: "Semgrep Report"
+                                )
                             )
-                        )
-                        archiveArtifacts artifacts: "${SEMGREP_REPORT}"
+                            archiveArtifacts artifacts: "${SEMGREP_REPORT}"
+                        }
                     }
                 }
             }
