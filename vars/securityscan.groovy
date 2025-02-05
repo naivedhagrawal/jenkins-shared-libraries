d351d88ea49ea38361897f9c2476d58aca8bea24
naivedh <naivedh@gmail.com>
2025-02-05 13:14:55 +0530

    1

--- vars/securityscan.groovy
+++ vars/securityscan.groovy
@@ -1,33 +1,34 @@
 def call(Map params = [:]) {
-    // Assign default values if not provided
     Boolean gitleak = params.get('gitleak', true)
     Boolean owaspdependency = params.get('owaspdependency', true)
     Boolean semgrep = params.get('semgrep', true)
 
     return {
-        if (gitleak) {
-            stage('Gitleaks Scan') {
-                steps {
-                    echo 'Running Gitleaks scan...'
-                    gitleakscan()()
+        stage('Security Scans') {
+            parallel {
+                if (gitleak) {
+                    stage('Gitleaks Scan') {
+                        steps {
+                            echo 'Running Gitleaks scan...'
+                            gitleakscan()()
+                        }
+                    }
                 }
-            }
-        }
-
-        if (owaspdependency) {
-            stage('OWASP Dependency Check') {
-                steps {
-                    echo 'Running OWASP Dependency scan...'
-                    owaspdependencycheck()()
+                if (owaspdependency) {
+                    stage('OWASP Dependency Check') {
+                        steps {
+                            echo 'Running OWASP Dependency scan...'
+                            owaspdependencycheck()()
+                        }
+                    }
                 }
-            }
-        }
-
-        if (semgrep) {
-            stage('Semgrep Scan') {
-                steps {
-                    echo 'Running Semgrep scan...'
-                    semgrepscan()()
+                if (semgrep) {
+                    stage('Semgrep Scan') {
+                        steps {
+                            echo 'Running Semgrep scan...'
+                            semgrepscan()()
+                        }
+                    }
                 }
             }
         }
