b141ee2eeb1c4f087fdf422f8c14903eba38c27a
naivedh <naivedh@gmail.com>
2025-02-05 22:24:05 +0530

    1

--- vars/trivyscan.groovy
+++ vars/trivyscan.groovy
@@ -10,29 +10,39 @@ def call() {
             string(name: 'git_URL', description: 'URL for Git Repo scan')
             string(name: 'image_name', description: 'Docker image name for scan')
         }
-            
+        
+        stages {
+            stage('VALIDATION') {
+                steps {
+                    script {
+                        if (!params.image_name && !params.git_URL) {
+                            error('Either image_name or git_URL parameter must be provided')
+                        }
+                    }
+                }
+            }
+
             stage('SCANNING USING TRIVY') {
                 steps {
                     container('trivy') {
                         script {
-                            switch (params.scanType) {
-                                case 'git_URL':
-                                    sh ""
-                                    break
-                                case 'image_name':
-                                    sh "trivy image"
-                                    sh "trivy repository"
-                                    sh "trivy convert --input report.json --output sarif > report.sarif"
-                                    break
+                            def trivy_report = 'trivy-report.json'
+                            def trivy_report_sarif = 'trivy-report.sarif'
+                            
+                            if (params.image_name) {
+                                sh "trivy image ${params.image_name} -f json -o ${trivy_report}"
+                                sh "trivy convert --format sarif --output ${trivy_report_sarif} ${trivy_report}"
+                            } else {
+                                sh "trivy repository ${params.git_URL} -f json -o ${trivy_report}"
+                                sh "trivy convert --format sarif --output ${trivy_report_sarif} ${trivy_report}"
                             }
+                            
+                            archiveArtifacts artifacts: trivy_report
+                            archiveArtifacts artifacts: trivy_report_html
                         }
-                        recordIssues(
-                            enabledForFailure: true,
-                            tool: sarif(pattern: "${ZAP_SARIF}", id: "zap-SARIF", name: "DAST Report"))
-                        archiveArtifacts artifacts: "${TRIVY_REPORT}"
-                        archiveArtifacts artifacts: "${TRIVY_REPORT_HTML}"
                     }
                 }
             }
         }
     }
+}
