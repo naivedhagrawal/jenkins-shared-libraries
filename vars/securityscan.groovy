9bbf792da078457ce678768144a78a77f4b9fac0
naivedh <naivedh@gmail.com>
2025-02-05 14:55:26 +0530

    1

--- vars/securityscan.groovy
+++ vars/securityscan.groovy
@@ -5,24 +5,27 @@ def call(Map params = [:]) {
             stage('Gitleaks Scan') {
                 when { expression { params.get('gitleak', true) } }
                 steps {
-                    echo 'Running Gitleaks scan...'
-                    gitleakscan()
+                    script {
+                        gitleakscan() // No extra ()
+                    }
                 }
             }
 
             stage('OWASP Dependency Check') {
                 when { expression { params.get('owaspdependency', true) } }
                 steps {
-                    echo 'Running OWASP Dependency scan...'
-                    owaspdependencycheck()
+                    script {
+                        owaspdependencycheck()
+                    }
                 }
             }
 
             stage('Semgrep Scan') {
                 when { expression { params.get('semgrep', true) } }
                 steps {
-                    echo 'Running Semgrep scan...'
-                    semgrepscan()
+                    script {
+                        semgrepscan()
+                    }
                 }
             }
         }
