1cdd220bceea94d2e20da0fd72bff4023eab60ca
naivedh <naivedh@gmail.com>
2025-02-05 15:09:02 +0530

    1

--- vars/securityscan.groovy
+++ vars/securityscan.groovy
@@ -12,23 +12,25 @@ def call(Map params = [:]) {
                 }
             }
             steps {
-                container('gitleak') {
-                    sh """
-                        gitleaks detect \
-                            --source=. \
-                            --report-path=${GITLEAKS_REPORT} \
-                            --report-format sarif \
-                            --exit-code=0
-                    """
-                    recordIssues(
-                        enabledForFailure: true,
-                        tool: sarif(
-                            pattern: "${GITLEAKS_REPORT}",
-                            id: "gitLeaks-SARIF",
-                            name: "GitLeaks-Report"
+                script {
+                    container('gitleak') {
+                        sh """
+                            gitleaks detect \
+                                --source=. \
+                                --report-path=${GITLEAKS_REPORT} \
+                                --report-format sarif \
+                                --exit-code=0
+                        """
+                        recordIssues(
+                            enabledForFailure: true,
+                            tool: sarif(
+                                pattern: "${GITLEAKS_REPORT}",
+                                id: "gitLeaks-SARIF",
+                                name: "GitLeaks-Report"
+                            )
                         )
-                    )
-                    archiveArtifacts artifacts: "${GITLEAKS_REPORT}"
+                        archiveArtifacts artifacts: "${GITLEAKS_REPORT}"
+                    }
                 }
             }
         }
@@ -43,23 +45,25 @@ def call(Map params = [:]) {
                 }
             }
             steps {
-                container('owasp') {
-                    withCredentials([string(credentialsId: 'NVD_API_KEY', variable: 'NVD_API_KEY')]) {
-                        sh """
-                            dependency-check --scan . \
-                                --format SARIF \
-                                --out ${OWASP_DEP_REPORT} \
-                                --nvdApiKey ${env.NVD_API_KEY}
-                        """
-                        recordIssues(
-                            enabledForFailure: true,
-                            tool: sarif(
-                                pattern: "${OWASP_DEP_REPORT}",
-                                id: "owasp-dependency-check-SARIF",
-                                name: "OWASP Dependency Check Report"
+                script {
+                    container('owasp') {
+                        withCredentials([string(credentialsId: 'NVD_API_KEY', variable: 'NVD_API_KEY')]) {
+                            sh """
+                                dependency-check --scan . \
+                                    --format SARIF \
+                                    --out ${OWASP_DEP_REPORT} \
+                                    --nvdApiKey ${env.NVD_API_KEY}
+                            """
+                            recordIssues(
+                                enabledForFailure: true,
+                                tool: sarif(
+                                    pattern: "${OWASP_DEP_REPORT}",
+                                    id: "owasp-dependency-check-SARIF",
+                                    name: "OWASP Dependency Check Report"
+                                )
                             )
-                        )
-                        archiveArtifacts artifacts: "${OWASP_DEP_REPORT}"
+                            archiveArtifacts artifacts: "${OWASP_DEP_REPORT}"
+                        }
                     }
                 }
             }
@@ -70,24 +74,26 @@ def call(Map params = [:]) {
         stage('Semgrep Scan') {
             agent {
                 kubernetes {
-                    yaml pod('semgrep','returntocorp/semgrep')
+                    yaml pod('semgrep', 'returntocorp/semgrep')
                     showRawYaml false
                 }
             }
             steps {
-                container('semgrep') {
-                    sh """
-                        semgrep --config=auto --sarif --output ${SEMGREP_REPORT} .
-                    """
-                    recordIssues(
-                        enabledForFailure: true,
-                        tool: sarif(
-                            pattern: "${SEMGREP_REPORT}",
-                            id: "semgrep-SARIF",
-                            name: "Semgrep Report"
+                script {
+                    container('semgrep') {
+                        sh """
+                            semgrep --config=auto --sarif --output ${SEMGREP_REPORT} .
+                        """
+                        recordIssues(
+                            enabledForFailure: true,
+                            tool: sarif(
+                                pattern: "${SEMGREP_REPORT}",
+                                id: "semgrep-SARIF",
+                                name: "Semgrep Report"
+                            )
                         )
-                    )
-                    archiveArtifacts artifacts: "${SEMGREP_REPORT}"
+                        archiveArtifacts artifacts: "${SEMGREP_REPORT}"
+                    }
                 }
             }
         }
