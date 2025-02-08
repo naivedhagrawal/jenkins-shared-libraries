351ea9b615cd837a50f7a7c46800d3d053e854dc
naivedhagrawal <naivedh@gmail.com>
2025-02-09 03:47:55 +0530

    1

--- vars/snykscan.groovy
+++ vars/snykscan.groovy
@@ -25,36 +25,38 @@ def call() {
                     }
                 }
                 steps {
-                    script {
-                        if (params.gitUrl?.trim()) {
-                            sh "snyk test --all-projects --sarif > snyk-sca.sarif"
-                            recordIssues(
-                                enabledForFailure: true,
-                                tool: sarif(pattern: "snyk-sca.sarif", id: "sca-SARIF", name: "SCA Report")
-                            )
+                    container('snyk') {
+                        script {
+                            if (params.gitUrl?.trim()) {
+                                sh "snyk test --all-projects --sarif > snyk-sca.sarif"
+                                recordIssues(
+                                    enabledForFailure: true,
+                                    tool: sarif(pattern: "snyk-sca.sarif", id: "sca-SARIF", name: "SCA Report")
+                                )
+                                
+                                sh "snyk code test --sarif > snyk-sast.sarif"
+                                recordIssues(
+                                    enabledForFailure: true,
+                                    tool: sarif(pattern: "snyk-sast.sarif", id: "sast-SARIF", name: "SAST Report")
+                                )
+                                
+                                sh "snyk iac test --sarif > snyk-iac.sarif"
+                                recordIssues(
+                                    enabledForFailure: true,
+                                    tool: sarif(pattern: "snyk-iac.sarif", id: "iac-SARIF", name: "IaC Security Report")
+                                )
+                            }
                             
-                            sh "snyk code test --sarif > snyk-sast.sarif"
-                            recordIssues(
-                                enabledForFailure: true,
-                                tool: sarif(pattern: "snyk-sast.sarif", id: "sast-SARIF", name: "SAST Report")
-                            )
-                            
-                            sh "snyk iac test --sarif > snyk-iac.sarif"
-                            recordIssues(
-                                enabledForFailure: true,
-                                tool: sarif(pattern: "snyk-iac.sarif", id: "iac-SARIF", name: "IaC Security Report")
-                            )
+                            if (params.imageName?.trim()) {
+                                sh "snyk container test ${params.imageName} --sarif > snyk-container.sarif"
+                                recordIssues(
+                                    enabledForFailure: true,
+                                    tool: sarif(pattern: "snyk-container.sarif", id: "container-SARIF", name: "Container Security Report")
+                                )
+                            }
                         }
-                        
-                        if (params.imageName?.trim()) {
-                            sh "snyk container test ${params.imageName} --sarif > snyk-container.sarif"
-                            recordIssues(
-                                enabledForFailure: true,
-                                tool: sarif(pattern: "snyk-container.sarif", id: "container-SARIF", name: "Container Security Report")
-                            )
-                        }
+                        archiveArtifacts artifacts: "snyk-*.sarif"
                     }
-                    archiveArtifacts artifacts: "snyk-*.sarif"
                 }
             }
         }
