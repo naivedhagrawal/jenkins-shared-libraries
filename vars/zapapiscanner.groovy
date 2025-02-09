f8c3ddf3880f1d5486a4277ec41b1dc0d44afdb3
naivedhagrawal <naivedh@gmail.com>
2025-02-09 14:35:00 +0530

    1

--- vars/zapapiscanner.groovy
+++ vars/zapapiscanner.groovy
@@ -10,8 +10,6 @@ def call() {
             ZAP_REPORT_HTML = 'zap-out.html'
             ZAP_REPORT_MD = 'zap-out.md'
             ZAP_SARIF = 'zap_report.sarif'
-            API_JSON_URL = "${params.apiJsonUrl?.trim()}"
-            API_FORMAT = "${params.apiFormat?.trim()}"
         }
 
         stages {
@@ -25,24 +23,18 @@ def call() {
                 steps {
                     container('zap') {
                         script {
-                            if (!API_JSON_URL || API_JSON_URL.trim() == '') {
+                            if (!params.apiJsonUrl?.trim()) {
                                 error('ERROR: URL for API definition JSON file cannot be empty.')
                             }
-                            if (!API_FORMAT || API_FORMAT.trim() == '') {
+                            if (!params.apiFormat?.trim()) {
                                 error('ERROR: API format must be specified (openapi, soap, graphql).')
                             }
                             
-                            // Download API JSON file with original name
-                            sh "curl -O '${API_JSON_URL}'"
-                            
-                            // Find the downloaded JSON file
-                            def apiJsonFile = sh(script: "ls *.json | head -n 1", returnStdout: true).trim()
-                            if (!apiJsonFile) {
-                                error('ERROR: No JSON file found after download.')
-                            }
+                            echo "API JSON URL: ${params.apiJsonUrl}"
+                            echo "API Format: ${params.apiFormat}"
                             
                             // Run ZAP API scan directly using the JSON file URL
-                            sh "zap-api-scan.py -t '${API_JSON_URL}' -f '${API_FORMAT}' -J '$ZAP_REPORT' -r '$ZAP_REPORT_HTML' -w '$ZAP_REPORT_MD' -I"
+                            sh "zap-api-scan.py -t '${params.apiJsonUrl}' -f '${params.apiFormat}' -J '$ZAP_REPORT' -r '$ZAP_REPORT_HTML' -w '$ZAP_REPORT_MD' -I"
                             sh 'mv /zap/wrk/${ZAP_REPORT} .' 
                             sh 'mv /zap/wrk/${ZAP_REPORT_HTML} .'
                             sh 'mv /zap/wrk/${ZAP_REPORT_MD} .'
@@ -52,7 +44,6 @@ def call() {
                     archiveArtifacts artifacts: "${ZAP_REPORT}"
                     archiveArtifacts artifacts: "${ZAP_REPORT_HTML}"
                     archiveArtifacts artifacts: "${ZAP_REPORT_MD}"
-                    archiveArtifacts artifacts: '*.json'  // Archive the downloaded API JSON file
                     
                     container('python') {
                         script {
