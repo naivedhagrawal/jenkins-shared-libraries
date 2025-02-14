f95fe952436d4a7026cab7ca4d02495079eea3dd
naivedh <naivedh@gmail.com>
2025-02-15 02:18:42 +0530

    1

--- vars/zapscanner.groovy
+++ vars/zapscanner.groovy
@@ -41,9 +41,9 @@ ZAP Command - Custom ZAP command execution''',
                     container('zap') {
                         script {
                             // Set system limits before running ZAP
-                            sh 'ulimit -u 100000'
-                            sh 'ulimit -n 1048576'
-                            
+                            sh 'ulimit -a' // Display current limits
+                            sh 'ulimit -n 1048576 || true' // Set open file limit, ignore errors
+
                             // Save the TARGET_URL to a file
                             writeFile file: 'target_url.txt', text: "Target URL: ${TARGET_URL}"
 
