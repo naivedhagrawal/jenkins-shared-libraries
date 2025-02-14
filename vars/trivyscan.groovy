7bf8440fcdb07738364d190dbfbb899d58242030
naivedh <naivedh@gmail.com>
2025-02-14 22:53:46 +0530

    1

--- vars/trivyscan.groovy
+++ vars/trivyscan.groovy
@@ -9,6 +9,9 @@ def call() {
         parameters {
             string(name: 'git_URL', description: 'URL for Git Repo scan')
             string(name: 'image_name', description: 'Docker image name for scan')
+            choice(name: 'registry_type', choices: ['public', 'private'], description: 'Registry type (public or private)')
+            string(name: 'docker_username', description: 'Docker Hub Username', defaultValue: '')
+            password(name: 'docker_password', description: 'Docker Hub Password', defaultValue: '')
         }
         
         stages {
@@ -17,6 +20,9 @@ def call() {
                     script {
                         if (!params.image_name && !params.git_URL) {
                             error('Either image_name or git_URL parameter must be provided')
+                        }
+                        if (params.git_URL && !params.git_URL.startsWith('https://')) {
+                            error('git_URL must start with https://')
                         }
                     }
                 }
@@ -28,10 +34,16 @@ def call() {
                         script {
                             def trivy_report_table = 'trivy-report.txt'
                             def trivy_report_sarif = 'trivy-report.sarif'
-                            
+                            def imageToScan = params.image_name
+
+                            if (params.registry_type == 'private') {
+                                sh "echo '${params.docker_password}' | docker login -u '${params.docker_username}' --password-stdin"
+                                imageToScan = "${params.docker_username}/${params.image_name}"
+                            }
+
                             if (params.image_name) {
-                                sh "trivy image ${params.image_name} -f table -o ${trivy_report_table} --debug"
-                                sh "trivy image ${params.image_name} -f sarif -o ${trivy_report_sarif} --debug"
+                                sh "trivy image ${imageToScan} -f table -o ${trivy_report_table} --debug"
+                                sh "trivy image ${imageToScan} -f sarif -o ${trivy_report_sarif} --debug"
                             } else {
                                 sh "trivy repository ${params.git_URL} -f table -o ${trivy_report_table} --debug"
                                 sh "trivy repository ${params.git_URL} -f sarif -o ${trivy_report_sarif} --debug"
