3f4561c8f115a985fc2f2397677dd668be763bd3
naivedh <naivedh@gmail.com>
2025-02-15 19:35:09 +0530

    1

--- vars/zapscanner.groovy
+++ vars/zapscanner.groovy
@@ -1,11 +1,10 @@
 def call() {
-    pipeline {
+ pipeline {
     agent {
         kubernetes {
             yaml zap()
             showRawYaml false
         }
-                }
     environment {
         ZAP_URL = 'http://zap.devops-tools.svc.cluster.local:8090'
         TARGET_URL = ''  // This will be populated at runtime
@@ -14,23 +13,35 @@ def call() {
         string(name: 'TARGET_URL', defaultValue: '', description: 'Target URL for ZAP scan')
     }
     stages {
-        stage('Start ZAP Scan') {
+        stage('Start ZAP Spider') {
             steps {
                 script {
                     if (!params.TARGET_URL) {
                         error("TARGET_URL is required.")
                     }
-                    
-                    // Triggering the ZAP scan through API
-                    echo "Starting ZAP scan on target: ${params.TARGET_URL}"
 
-                    // Initiate a new scan using the ZAP API without API key
+                    // Start ZAP Spider to crawl the target URL
+                    echo "Starting ZAP spidering on target: ${params.TARGET_URL}"
+                    def spiderUrl = "${ZAP_URL}/JSON/spider/action/scan"
+                    def spiderResponse = sh(script: """
+                        curl -s -X GET "${spiderUrl}?url=${params.TARGET_URL}"
+                    """, returnStdout: true)
+
+                    echo "Spider response: ${spiderResponse}"
+                }
+            }
+        }
+        stage('Start ZAP Scan') {
+            steps {
+                script {
+                    // Triggering the ZAP scan after the spider
+                    echo "Starting ZAP scan on target: ${params.TARGET_URL}"
                     def scanUrl = "${ZAP_URL}/JSON/ascan/action/scan"
-                    def response = sh(script: """
+                    def scanResponse = sh(script: """
                         curl -s -X GET "${scanUrl}?url=${params.TARGET_URL}&maxDepth=5&maxChildren=10"
                     """, returnStdout: true)
 
-                    echo "Scan initiated, response: ${response}"
+                    echo "Scan initiated, response: ${scanResponse}"
                 }
             }
         }
@@ -78,5 +89,5 @@ def call() {
         }
     }
 }
-
+}
 }
