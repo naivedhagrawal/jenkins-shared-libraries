ab48bbea071ff5bdafc6a6c6e625ec8c32cd4030
naivedhagrawal <113852812+naivedhagrawal@users.noreply.github.com>
2025-03-14 18:22:47 +0530

    Update report archiving process in zapscanheadless.groovy for improved artifact handling

--- vars/zapscanheadless.groovy
+++ vars/zapscanheadless.groovy
@@ -68,10 +68,10 @@ def call() {
                             sh "curl -s \"${ZAP_URL}/OTHER/core/other/htmlreport/?title=Enhanced+ZAP+Report\" -o zap-enhanced-report.html"
                             sh "curl -s \"${ZAP_URL}/OTHER/core/other/htmlreport/?title=ZAP%20Security%20Report&template=traditional\" -o zap-traditional-report.html"
                             sh "curl -s \"${ZAP_URL}/JSON/reports/action/generate/?title=ZAP%20Security%20Report&template=modern&reportDir=/zap/reports/&reportFileName=modern-report.html\""
-                            sh "cp /zap/reports/modern-report.html ."
+                            sh "cp -r /zap/reports/modern-report.html /zap/reports/modern-report ."
                             sh 'ls -l'
                             echo "Archiving Enhanced ZAP Reports..."
-                            archiveArtifacts artifacts: 'zap-traditional-report.html, zap-enhanced-report.html, modern-report.html, zap-report.json', fingerprint: true
+                            archiveArtifacts artifacts: 'modern-report.html, modern-report/**', fingerprint: true
                         }
                     }
                 }
