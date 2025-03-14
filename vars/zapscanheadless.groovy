aafa70a5b022b38dd8dd6f04dd63a93f7c8daa55
naivedhagrawal <113852812+naivedhagrawal@users.noreply.github.com>
2025-03-14 09:04:59 +0530

    Update report generation to include modern HTML format and adjust archived artifacts

--- vars/zapscanheadless.groovy
+++ vars/zapscanheadless.groovy
@@ -67,10 +67,10 @@ def call() {
                             sh "curl -s \"${ZAP_URL}/OTHER/core/other/jsonreport/\" -o zap-report.json"
                             sh "curl -s \"${ZAP_URL}/OTHER/core/other/htmlreport/?title=Enhanced+ZAP+Report\" -o zap-enhanced-report.html"
                             sh "curl -s \"${ZAP_URL}/OTHER/core/other/htmlreport/?title=ZAP%20Security%20Report&template=traditional\" -o zap-traditional-report.html"
-                            sh "curl -s \"${ZAP_URL}/OTHER/core/other/markdownreport/\" -o zap-report.md"
+                            sh "curl -s \"${ZAP_URL}/JSON/reports/action/generate/?title=ZAP%20Security%20Report&template=modern&reportDir=/home/zap/&reportFileName=modern-report.html\""
 
                             echo "Archiving Enhanced ZAP Reports..."
-                            archiveArtifacts artifacts: 'zap-traditional-report.html, zap-enhanced-report.html, zap-report.md, zap-report.json', fingerprint: true
+                            archiveArtifacts artifacts: 'zap-traditional-report.html, zap-enhanced-report.html, modern-report.html, zap-report.json', fingerprint: true
                         }
                     }
                 }
