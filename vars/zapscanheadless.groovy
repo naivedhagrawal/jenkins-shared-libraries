336ef5032c4f601963cb2c308e17f8884954862a
naivedhagrawal <113852812+naivedhagrawal@users.noreply.github.com>
2025-03-14 11:03:03 +0530

    Update report generation path and file locations in zapscanheadless.groovy

--- vars/zapscanheadless.groovy
+++ vars/zapscanheadless.groovy
@@ -67,9 +67,9 @@ def call() {
                             sh "curl -s \"${ZAP_URL}/OTHER/core/other/jsonreport/\" -o zap-report.json"
                             sh "curl -s \"${ZAP_URL}/OTHER/core/other/htmlreport/?title=Enhanced+ZAP+Report\" -o zap-enhanced-report.html"
                             sh "curl -s \"${ZAP_URL}/OTHER/core/other/htmlreport/?title=ZAP%20Security%20Report&template=traditional\" -o zap-traditional-report.html"
-                            sh "curl -s \"${ZAP_URL}/JSON/reports/action/generate/?title=ZAP%20Security%20Report&template=modern&reportFileName=modern-report.html\""
+                            sh "curl -s \"${ZAP_URL}/JSON/reports/action/generate/?title=ZAP%20Security%20Report&template=modern&reportDir=/home/zap/&reportFileName=modern-report.html\""
                             
-                            sh "cp /zap/wrk/modern-report.html ."
+                            sh "cp /home/zap/modern-report.html ."
                             echo "Archiving Enhanced ZAP Reports..."
                             archiveArtifacts artifacts: 'zap-traditional-report.html, zap-enhanced-report.html, modern-report.html, zap-report.json', fingerprint: true
                         }
