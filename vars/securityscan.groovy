404f5b49ca8ebd4688697afd34f98b9a5963c2fb
naivedh <naivedh@gmail.com>
2025-03-27 08:41:50 +0530

    1

--- vars/securityscan.groovy
+++ vars/securityscan.groovy
@@ -17,6 +17,40 @@ def call(Map params = [gitleak: true, owaspdependency: true, semgrep: true, chec
         agent none
 
         stages {
+            
+            stage('SonarQube Scan') {
+                when { expression { params.sonarqube } }
+                agent {
+                    kubernetes {
+                        yaml pod('sonarqube', 'sonarsource/sonar-scanner-cli:latest')
+                        showRawYaml false
+                    }
+                }
+                steps {
+                    script {
+                        container('sonarqube') {
+                            checkout scm
+                            sh """
+                                sonar-scanner \
+                                -Dsonar.host.url=http://localhost:9000 \
+                                -Dsonar.projectKey=${env.JOB_NAME} \
+                                -Dsonar.sources=. \
+                                -Dsonar.qualitygate.wait=true
+                            """
+                            recordIssues(
+                                enabledForFailure: true,
+                                tool: sonarQubeScanner(
+                                    pattern: '**/sonar-report.json',
+                                    id: 'SonarQube-Analysis',
+                                    name: 'SonarQube Report'
+                                )
+                            )
+                            archiveArtifacts artifacts: '**/sonar-report.json'
+                        }
+                    }
+                }
+            }
+
             stage('Gitleak Check') {
                 when { expression { params.gitleak } }
                 agent {
@@ -86,39 +120,6 @@ def call(Map params = [gitleak: true, owaspdependency: true, semgrep: true, chec
                     }
                 }
             }
-            stage('SonarQube Scan') {
-                when { expression { params.sonarqube } }
-                agent {
-                    kubernetes {
-                        yaml pod('sonarqube', 'sonarsource/sonar-scanner-cli:latest')
-                        showRawYaml false
-                    }
-                }
-                steps {
-                    script {
-                        container('sonarqube') {
-                            checkout scm
-                            sh """
-                                sonar-scanner \
-                                -Dsonar.host.url=http://localhost:9000 \
-                                -Dsonar.projectKey=${env.JOB_NAME} \
-                                -Dsonar.sources=. \
-                                -Dsonar.qualitygate.wait=true
-                            """
-                            recordIssues(
-                                enabledForFailure: true,
-                                tool: sonarQubeScanner(
-                                    pattern: '**/sonar-report.json',
-                                    id: 'SonarQube-Analysis',
-                                    name: 'SonarQube Report'
-                                )
-                            )
-                            archiveArtifacts artifacts: '**/sonar-report.json'
-                        }
-                    }
-                }
-            }
-
 
             stage('Semgrep Scan') {
                 when { expression { params.semgrep } }
