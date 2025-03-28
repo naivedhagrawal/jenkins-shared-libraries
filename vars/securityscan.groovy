ea98e0c4ad9fdf2c37f9c4cf878143f3c0eef7d9
naivedh <naivedh@gmail.com>
2025-03-28 15:44:34 +0530

    1

--- vars/securityscan.groovy
+++ vars/securityscan.groovy
@@ -1,15 +1,34 @@
+/* @Library('k8s-shared-lib') _
+securityscan(
+    params: [
+        gitleak: true,
+        owaspdependency: true,
+        semgrep: true,
+        checkov: true,
+        GIT_URL: 'https://github.com/naivedhagrawal/devops_tools_kubernetes.git',
+        GIT_BRANCH: 'main'
+    ]
+)
+*/
+
 import com.mycompany.utils.PodGenerator
 
-def call(Map params = [gitleak: true, owaspdependency: true, semgrep: true, checkov: true, GIT_URL: '', GIT_BRANCH: '']) {
+def call(Map params = [:]) {
+    // Retrieve parameters with safe defaults
+    def GIT_URL = params.get('GIT_URL', '')
+    def GIT_BRANCH = params.get('GIT_BRANCH', '')
+
+    if (!GIT_URL || !GIT_BRANCH) {
+        error "🚨 GIT_URL or GIT_BRANCH is not set!"
+    }
+
     def GITLEAKS_REPORT = 'gitleaks-report'
     def OWASP_DEP_REPORT = 'owasp-dep-report'
     def SEMGREP_REPORT = 'semgrep-report'
     def CHECKOV_REPORT = 'results.sarif'
     def SEMGREP_CREDENTIALS_ID = 'SEMGREP_KEY'
-    GIT_URL = params.GIT_URL
-    GIT_BRANCH = params.GIT_BRANCH
 
-    // Define the containers. Add git container for cloning the repository
+    // Define the containers for the Kubernetes pod
     def containers = [
         [name: 'git', image: 'alpine/git:latest'],
         [name: 'gitleak', image: 'zricethezav/gitleaks'],
@@ -18,7 +37,7 @@ def call(Map params = [gitleak: true, owaspdependency: true, semgrep: true, chec
         [name: 'checkov', image: 'bridgecrew/checkov:latest']
     ]
 
-    // Generate the pod YAML by calling the function from the shared library
+    // Generate the pod YAML from the shared library
     def podYaml = PodGenerator.generatePodYaml(containers)
 
     pipeline {
@@ -32,77 +51,87 @@ def call(Map params = [gitleak: true, owaspdependency: true, semgrep: true, chec
             stage('Git Clone') {
                 steps {
                     container('git') {
-                        sh '''
-                            echo "GIT_URL: ${GIT_URL}"
-                            echo "GIT_BRANCH: ${GIT_BRANCH}"
-                            if [ -z "${GIT_URL}" ] || [ -z "${GIT_BRANCH}" ]; then
-                                echo "Error: GIT_URL or GIT_BRANCH is not set"
-                                exit 1
-                            fi
-                            echo "Cloning repository..."
-                            git config --global --add safe.directory $PWD
-                            git clone --depth=1 --branch ${GIT_BRANCH} ${GIT_URL} .
-                        '''
+                        withEnv(["GIT_URL=${GIT_URL}", "GIT_BRANCH=${GIT_BRANCH}"]) {
+                            sh '''
+                                echo "GIT_URL: $GIT_URL"
+                                echo "GIT_BRANCH: $GIT_BRANCH"
+                                echo "Cloning repository..."
+                                git config --global --add safe.directory $PWD
+                                git clone --depth=1 --branch $GIT_BRANCH $GIT_URL /source
+                            '''
+                        }
                     }
                 }
             }
 
-
             stage('Gitleak Check') {
                 when { expression { params.gitleak } }
                 steps {
-                    sh "gitleaks detect --source=/source --report-path=${GITLEAKS_REPORT}.sarif --report-format sarif --exit-code=0"
-                    recordIssues(
-                        enabledForFailure: true,
-                        tool: sarif(
-                            pattern: "${GITLEAKS_REPORT}.sarif",
-                            id: "Git-Leaks",
-                            name: "Secret Scanning Report"
+                    container('gitleak') {
+                        sh '''
+                            echo "Running Gitleaks scan..."
+                            gitleaks detect --source=/source --report-path=/source/${GITLEAKS_REPORT}.sarif --report-format sarif --exit-code=0
+                        '''
+                        recordIssues(
+                            enabledForFailure: true,
+                            tool: sarif(
+                                pattern: "/source/${GITLEAKS_REPORT}.sarif",
+                                id: "Git-Leaks",
+                                name: "Secret Scanning Report"
+                            )
                         )
-                    )
-                    archiveArtifacts artifacts: "${GITLEAKS_REPORT}.*"
+                        archiveArtifacts artifacts: "/source/${GITLEAKS_REPORT}.*"
+                    }
                 }
             }
 
             stage('OWASP Dependency Check') {
                 when { expression { params.owaspdependency } }
                 steps {
-                    sh """
-                        mkdir -p reports
-                        /usr/share/dependency-check/bin/dependency-check.sh --scan /source \
-                            --format "SARIF"  \
-                            --exclude "**/*.zip" \
-                            --out "reports/"
-                        
-                        mv reports/dependency-check-report.sarif ${OWASP_DEP_REPORT}.sarif
-                    """
-                    recordIssues(
-                        enabledForFailure: true,
-                        tool: sarif(
-                            pattern: "${OWASP_DEP_REPORT}.sarif",
-                            id: "Owasp-Dependency-Check",
-                            name: "Dependency Check Report"
+                    container('owasp') {
+                        sh '''
+                            echo "Running OWASP Dependency Check..."
+                            mkdir -p /source/reports
+                            /usr/share/dependency-check/bin/dependency-check.sh --scan /source \
+                                --format "SARIF"  \
+                                --exclude "**/*.zip" \
+                                --out "/source/reports/"
+                            
+                            mv /source/reports/dependency-check-report.sarif /source/${OWASP_DEP_REPORT}.sarif
+                        '''
+                        recordIssues(
+                            enabledForFailure: true,
+                            tool: sarif(
+                                pattern: "/source/${OWASP_DEP_REPORT}.sarif",
+                                id: "Owasp-Dependency-Check",
+                                name: "Dependency Check Report"
+                            )
                         )
-                    )
-                    archiveArtifacts artifacts: "${OWASP_DEP_REPORT}.*"
+                        archiveArtifacts artifacts: "/source/${OWASP_DEP_REPORT}.*"
+                    }
                 }
             }
 
             stage('Semgrep Scan') {
                 when { expression { params.semgrep } }
                 steps {
-                    withCredentials([string(credentialsId: SEMGREP_CREDENTIALS_ID, variable: 'SEMGREP_KEY')]) {
-                        sh "mkdir -p reports"
-                        sh "semgrep --config=auto --sarif --output reports/semgrep.sarif /source"
-                        recordIssues(
-                            enabledForFailure: true,
-                            tool: sarif(
-                                pattern: "reports/semgrep.sarif",
-                                id: "SEMGREP-SAST",
-                                name: "SAST Report"
+                    container('semgrep') {
+                        withCredentials([string(credentialsId: SEMGREP_CREDENTIALS_ID, variable: 'SEMGREP_KEY')]) {
+                            sh '''
+                                echo "Running Semgrep Scan..."
+                                mkdir -p /source/reports
+                                semgrep --config=auto --sarif --output /source/reports/semgrep.sarif /source || true
+                            '''
+                            recordIssues(
+                                enabledForFailure: true,
+                                tool: sarif(
+                                    pattern: "/source/reports/semgrep.sarif",
+                                    id: "SEMGREP-SAST",
+                                    name: "SAST Report"
+                                )
                             )
-                        )
-                        archiveArtifacts artifacts: "reports/semgrep.*"
+                            archiveArtifacts artifacts: "/source/reports/semgrep.*"
+                        }
                     }
                 }
             }
@@ -110,16 +139,21 @@ def call(Map params = [gitleak: true, owaspdependency: true, semgrep: true, chec
             stage('Checkov Scan') {
                 when { expression { params.checkov } }
                 steps {
-                    sh "checkov --directory /source --output sarif || true"
-                    recordIssues(
-                        enabledForFailure: true,
-                        tool: sarif(
-                            pattern: "${CHECKOV_REPORT}",
-                            id: "Checkov-IaC",
-                            name: "IAC Test Report"
+                    container('checkov') {
+                        sh '''
+                            echo "Running Checkov Scan..."
+                            checkov --directory /source --output sarif --output-file-path /source/${CHECKOV_REPORT} || true
+                        '''
+                        recordIssues(
+                            enabledForFailure: true,
+                            tool: sarif(
+                                pattern: "/source/${CHECKOV_REPORT}",
+                                id: "Checkov-IaC",
+                                name: "IAC Test Report"
+                            )
                         )
-                    )
-                    archiveArtifacts artifacts: "${CHECKOV_REPORT}"
+                        archiveArtifacts artifacts: "/source/${CHECKOV_REPORT}"
+                    }
                 }
             }
         }
