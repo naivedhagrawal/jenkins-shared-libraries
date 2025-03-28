002c3c6d297701395a2ba473e89de341be3a4362
naivedh <naivedh@gmail.com>
2025-03-28 12:08:13 +0530

    1

---
+++ vars/pod copy.groovy
@@ -0,0 +1,22 @@
+def call(String name = 'jnlp', String image = 'jenkins/inbound-agent:latest') {
+    return """
+apiVersion: v1
+kind: Pod
+spec:
+  volumes:
+    - name: zap-reports
+      hostPath:
+        path: /home/data/zap-reports
+        type: DirectoryOrCreate
+  containers:
+    - name: ${name}
+      image: "${image}"
+      command:
+        - sleep
+        - infinity
+      tty: true
+      volumeMounts:
+        - name: zap-reports
+          mountPath: /zap/reports
+    """
+}
--- vars/securityscan.groovy
+++ vars/securityscan.groovy
@@ -12,37 +12,80 @@ def call(Map params = [gitleak: true, owaspdependency: true, semgrep: true, chec
     def OWASP_DEP_REPORT = 'owasp-dep-report'
     def SEMGREP_REPORT = 'semgrep-report'
     def CHECKOV_REPORT = 'results.sarif'
+    def WORKSPACE_DIR = '/workspace'  // Shared workspace path
 
     pipeline {
         agent none
 
         stages {
+            stage('Checkout') {
+                agent {
+                    kubernetes {
+                        yaml """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+    - name: checkout
+      image: jenkins/inbound-agent:latest
+      command:
+        - cat
+      tty: true
+      volumeMounts:
+        - name: workspace-volume
+          mountPath: ${WORKSPACE_DIR}
+  volumes:
+    - name: workspace-volume
+      persistentVolumeClaim:
+        claimName: jenkins-workspace-pvc
+"""
+                        defaultContainer 'checkout'
+                    }
+                }
+                steps {
+                    dir(WORKSPACE_DIR) {
+                        checkout scm
+                    }
+                }
+            }
 
             stage('Gitleak Check') {
                 when { expression { params.gitleak } }
                 agent {
                     kubernetes {
-                        yaml pod('gitleak', 'zricethezav/gitleaks')
-                        showRawYaml false
+                        yaml """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+    - name: gitleak
+      image: zricethezav/gitleaks
+      command:
+        - cat
+      tty: true
+      volumeMounts:
+        - name: workspace-volume
+          mountPath: ${WORKSPACE_DIR}
+  volumes:
+    - name: workspace-volume
+      persistentVolumeClaim:
+        claimName: jenkins-workspace-pvc
+"""
+                        defaultContainer 'gitleak'
                     }
                 }
                 steps {
-                    script {
-                        container('gitleak') {
-                            checkout scm
-                            sh "gitleaks detect --source=. --report-path=${GITLEAKS_REPORT}.sarif --report-format sarif --exit-code=0"
-                            /*sh "gitleaks detect --source=. --report-path=${GITLEAKS_REPORT}.json --report-format json --exit-code=0"
-                            sh "gitleaks detect --source=. --report-path=${GITLEAKS_REPORT}.csv --report-format csv --exit-code=0"*/
-                            recordIssues(
-                                enabledForFailure: true,
-                                tool: sarif(
-                                    pattern: "${GITLEAKS_REPORT}.sarif",
-                                    id: "Git-Leaks",
-                                    name: "Secret Scanning Report"
-                                )
+                    dir(WORKSPACE_DIR) {
+                        sh "gitleaks detect --source=. --report-path=${GITLEAKS_REPORT}.sarif --report-format sarif --exit-code=0"
+                        recordIssues(
+                            enabledForFailure: true,
+                            tool: sarif(
+                                pattern: "${GITLEAKS_REPORT}.sarif",
+                                id: "Git-Leaks",
+                                name: "Secret Scanning Report"
                             )
-                            archiveArtifacts artifacts: "${GITLEAKS_REPORT}.*"
-                        }
+                        )
+                        archiveArtifacts artifacts: "${GITLEAKS_REPORT}.*"
                     }
                 }
             }
@@ -51,39 +94,53 @@ def call(Map params = [gitleak: true, owaspdependency: true, semgrep: true, chec
                 when { expression { params.owaspdependency } }
                 agent {
                     kubernetes {
-                        yaml pod('owasp', 'owasp/dependency-check-action:latest')
-                        showRawYaml false
+                        yaml """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+    - name: owasp
+      image: owasp/dependency-check-action:latest
+      command:
+        - cat
+      tty: true
+      volumeMounts:
+        - name: workspace-volume
+          mountPath: ${WORKSPACE_DIR}
+  volumes:
+    - name: workspace-volume
+      persistentVolumeClaim:
+        claimName: jenkins-workspace-pvc
+"""
+                        defaultContainer 'owasp'
                     }
                 }
                 steps {
-                    script {
-                        container('owasp') {
-                            checkout scm
-                            sh """
-                                mkdir -p reports
-                                /usr/share/dependency-check/bin/dependency-check.sh --scan . \
-                                    --format "SARIF" \
-                                    --format "JSON" \
-                                    --format "CSV" \
-                                    --format "XML" \
-                                    --exclude "**/*.zip" \
-                                    --out "reports/"
-                                
-                                mv reports/dependency-check-report.sarif ${OWASP_DEP_REPORT}.sarif
-                                mv reports/dependency-check-report.json ${OWASP_DEP_REPORT}.json
-                                mv reports/dependency-check-report.csv ${OWASP_DEP_REPORT}.csv
-                                mv reports/dependency-check-report.xml ${OWASP_DEP_REPORT}.xml
-                            """
-                            recordIssues(
-                                enabledForFailure: true,
-                                tool: owaspDependencyCheck(
-                                    pattern: "${OWASP_DEP_REPORT}.json",
-                                    id: "Owasp-Dependency-Check",
-                                    name: "Dependency Check Report"
-                                )
+                    dir(WORKSPACE_DIR) {
+                        sh """
+                            mkdir -p reports
+                            /usr/share/dependency-check/bin/dependency-check.sh --scan . \
+                                --format "SARIF" \
+                                --format "JSON" \
+                                --format "CSV" \
+                                --format "XML" \
+                                --exclude "**/*.zip" \
+                                --out "reports/"
+                            
+                            mv reports/dependency-check-report.sarif ${OWASP_DEP_REPORT}.sarif
+                            mv reports/dependency-check-report.json ${OWASP_DEP_REPORT}.json
+                            mv reports/dependency-check-report.csv ${OWASP_DEP_REPORT}.csv
+                            mv reports/dependency-check-report.xml ${OWASP_DEP_REPORT}.xml
+                        """
+                        recordIssues(
+                            enabledForFailure: true,
+                            tool: owaspDependencyCheck(
+                                pattern: "${OWASP_DEP_REPORT}.json",
+                                id: "Owasp-Dependency-Check",
+                                name: "Dependency Check Report"
                             )
-                            archiveArtifacts artifacts: "${OWASP_DEP_REPORT}.*"
-                        }
+                        )
+                        archiveArtifacts artifacts: "${OWASP_DEP_REPORT}.*"
                     }
                 }
             }
@@ -92,29 +149,41 @@ def call(Map params = [gitleak: true, owaspdependency: true, semgrep: true, chec
                 when { expression { params.semgrep } }
                 agent {
                     kubernetes {
-                        yaml pod('semgrep', 'returntocorp/semgrep:latest')
-                        showRawYaml false
+                        yaml """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+    - name: semgrep
+      image: returntocorp/semgrep:latest
+      command:
+        - cat
+      tty: true
+      volumeMounts:
+        - name: workspace-volume
+          mountPath: ${WORKSPACE_DIR}
+  volumes:
+    - name: workspace-volume
+      persistentVolumeClaim:
+        claimName: jenkins-workspace-pvc
+"""
+                        defaultContainer 'semgrep'
                     }
                 }
                 steps {
-                    script {
-                        container('semgrep') {
-                            checkout scm
-                            withCredentials([string(credentialsId: 'SEMGREP_KEY', variable: 'SEMGREP_KEY')]) {
-                                sh "mkdir -p reports"
-                                sh "semgrep --config=auto --sarif --output reports/semgrep.sarif ."
-                                /*sh "semgrep --config=auto --json --output reports/semgrep.json ."
-                                sh "semgrep --config=auto --verbose --output reports/semgrep.txt ."*/
-                                archiveArtifacts artifacts: "reports/semgrep.*"
-                                recordIssues(
-                                    enabledForFailure: true,
-                                    tool: sarif(
-                                        pattern: "reports/semgrep.sarif",
-                                        id: "SEMGREP-SAST",
-                                        name: "SAST Report"
-                                    )
+                    dir(WORKSPACE_DIR) {
+                        withCredentials([string(credentialsId: 'SEMGREP_KEY', variable: 'SEMGREP_KEY')]) {
+                            sh "mkdir -p reports"
+                            sh "semgrep --config=auto --sarif --output reports/semgrep.sarif ."
+                            archiveArtifacts artifacts: "reports/semgrep.*"
+                            recordIssues(
+                                enabledForFailure: true,
+                                tool: sarif(
+                                    pattern: "reports/semgrep.sarif",
+                                    id: "SEMGREP-SAST",
+                                    name: "SAST Report"
                                 )
-                            }
+                            )
                         }
                     }
                 }
@@ -124,25 +193,39 @@ def call(Map params = [gitleak: true, owaspdependency: true, semgrep: true, chec
                 when { expression { params.checkov } }
                 agent {
                     kubernetes {
-                        yaml pod('checkov', 'bridgecrew/checkov:latest')
-                        showRawYaml false
+                        yaml """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+    - name: checkov
+      image: bridgecrew/checkov:latest
+      command:
+        - cat
+      tty: true
+      volumeMounts:
+        - name: workspace-volume
+          mountPath: ${WORKSPACE_DIR}
+  volumes:
+    - name: workspace-volume
+      persistentVolumeClaim:
+        claimName: jenkins-workspace-pvc
+"""
+                        defaultContainer 'checkov'
                     }
                 }
                 steps {
-                    script {
-                        container('checkov') {
-                            checkout scm
-                            sh "checkov --directory . --output sarif || true"
-                            recordIssues(
-                                enabledForFailure: true,
-                                tool: sarif(
-                                    pattern: "${CHECKOV_REPORT}",
-                                    id: "Checkov-IaC",
-                                    name: "IAC Test Report"
-                                )
+                    dir(WORKSPACE_DIR) {
+                        sh "checkov --directory . --output sarif || true"
+                        recordIssues(
+                            enabledForFailure: true,
+                            tool: sarif(
+                                pattern: "${CHECKOV_REPORT}",
+                                id: "Checkov-IaC",
+                                name: "IAC Test Report"
                             )
-                            archiveArtifacts artifacts: "${CHECKOV_REPORT}"
-                        }
+                        )
+                        archiveArtifacts artifacts: "${CHECKOV_REPORT}"
                     }
                 }
             }
